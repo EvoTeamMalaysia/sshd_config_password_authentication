ad79a5b1dd2d739ab50420e887f42971599c39e7
KuroShion <66388008+EvoTeamMalaysia@users.noreply.github.com>
2023-06-08 23:47:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 wget https://raw.githubusercontent.com/EvoTeamMalaysia/sshd_config_password_authentication/main/sshd_config
 mv sshd_config /etc/ssh/sshd_config
-systemctl restart ssh/sshd_config
-
+systemctl restart ssh
 echo Done!!!
 pause
