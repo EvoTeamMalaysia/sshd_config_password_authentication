636c04213998e73a7e194d34b7e69c53d8a8a040
KuroShion <66388008+EvoTeamMalaysia@users.noreply.github.com>
2023-06-14 09:47:07 +0800

    Update run.sh
    Changes On Error Lines

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 wget https://raw.githubusercontent.com/EvoTeamMalaysia/sshd_config_password_authentication/main/sshd_config
-
-sudo mv sshd_config /etc/ssh/sshd_config
-
-systemctl restart ssh
-
+sudo mv sshd_config /etc/ssh/
+sudo systemctl restart ssh
 echo Done!!!
